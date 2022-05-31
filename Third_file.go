a6f4e90f1d6153452b065e7fca96f9bde12a8bd2
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-01 02:52:07 +0430

    comments

---
+++ Third_file.go
@@ -0,0 +1,20 @@
+package main
+
+//This file is written for instruction only
+
+//import "fmt"
+
+//var outer = []string{"A"}
+
+//func MF(inner[]string){
+
+//	inner[0] = "B"
+
+//}
+
+//MF(outer)
+
+//output :
+
+//value inner = B
+//value outer = A
