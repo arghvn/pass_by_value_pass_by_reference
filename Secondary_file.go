d67ab626fb96b796427cd71c5e6e395f5a989113
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-01 02:43:27 +0430

    comments

---
+++ Secondary_file.go
@@ -0,0 +1,37 @@
+package main
+
+//The term pass by reference means that there is generally
+// a value of this variable and all operations refer to this variable.
+// All operations know where this value is.
+
+//This file is written for instruction only
+
+// an example of pass by Reference :
+
+//import "fmt"
+
+//var outer = "A"
+
+//func MF(inner *string){
+
+//The MF function here is a variable that receives a
+//pointer and a string. In fact,
+// the address that points to the string.
+
+//	*inner = "B"
+
+//Inside the function of existence * and inner means go to the value of inner.
+//inner is an address. Change the value of inner into memory and put B.
+
+//}
+
+//Finally we call the function.
+
+//Existence & means create an address from the outer variable and give it an address.
+
+//MF(&outer)
+
+//output :
+
+//value of inner = B
+//value of outer = B
