5638396adbe1b3a39700d9da815ff5efc502ee63
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-06-01 02:24:40 +0430

    comments

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+//This file is written for instruction only
+
+// an example of pass by value :
+
+//import "fmt"
+
+//The initial value is B.
+
+//Newly assigned variables are copied and the original value does not change.
+
+//var outer_str = "A"
+
+//func MF(mystr string) {
+
+//mystr = "B"
+
+//}
+
+//mf(outer_str)
+
+// output :
+
+//value of inner = B
+//value of  outer = A
+
+//In general, pass by value means a copy of it is made and this copied
+//value is given to the function.
+//Then the object performs the relevant operation on this copied value
+// and keeps the original value unchanged.
